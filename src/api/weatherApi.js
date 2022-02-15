c3686f81369653201a85d564fc4b44ce4fd1549b
trekvartista <isken_150502@mail.ru>
2022-02-15 22:42:11 +0600

    api -> weatherApi

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,17 +1,5 @@
 import Axios from "axios";
 
-// var options = {
-//   method: 'GET',
-//   url: 'https://weatherapi-com.p.rapidapi.com/ip.json',
-//   params: {q: '<REQUIRED>'},
-//   headers: {
-//     'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com',
-//     'x-rapidapi-key': 'SIGN-UP-FOR-KEY'
-//   }
-// };
-
-// http://api.weatherapi.com/v1/current.json?key=806aa4362e50400e988173920221302&q=bishkek&aqi=no
-
 const axios = Axios.create({
     baseURL: 'http://api.weatherapi.com/v1',
     params: {
