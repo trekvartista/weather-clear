6f64add5b315b53fcecbfbd0ee9567bd204439cf
trekvartista <isken_150502@mail.ru>
2022-02-15 22:43:47 +0600

    set default place (depends on IP) + beautify page

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import { weatherAPI } from '../../api/api'
+import React, { useEffect, useState } from 'react'
+import { weatherAPI } from '../../api/weatherApi'
+import { ipAPI } from '../../api/ipApi'
 import s from './Weather.module.css'
 
 function Weather() {
@@ -16,42 +17,66 @@ function Weather() {
         }
     }
 
-    const [input, setInput] = useState()
+    const [input, setInput] = useState('')
     const [location, setLocation] = useState(initialLocation)
     const [weather, setWeather] = useState(initialWeather)
 
-    const handleEvent = async (event) => {
+    useEffect(() => {
+
+        const setDefaultLocation = async () => {
+
+            let response = await ipAPI.getCurrentIP()
+            const currentIP = response.data.ip
+
+            response = await ipAPI.getIpInfo(currentIP)
+            setInput(response.data.city)
+            setData(response.data.city)
+        }
+
+        if (!input) {
+            setDefaultLocation()
+        }
+    }, [input])
+
+    const setData = async (input) => {
+
+        const response = await weatherAPI.requestCurrentWeather(input)
+            
+        // TODO: error handling
+        // debugger
+
+        const {country, localtime, name} = response.data.location
+        setLocation({country, localtime, name})
+
+        const {condition, temp_c} = response.data.current
+        setWeather({condition, temp_c})
+        // console.log(response.data)
+    }
+
+    const handleEvent = (event) => {
         if (event.keyCode === 13) {
             event.preventDefault();
 
-            const response = await weatherAPI.requestCurrentWeather(input)
-            
-            // TODO: error handling
-            // debugger
-
-            const {country, localtime, name} = response.data.location
-            setLocation({country, localtime, name})
-
-            const {condition, temp_c} = response.data.current
-            setWeather({condition, temp_c})
-            console.log(response.data)
+            setData(input)
         }
     }
 
     return (
         <div className={s.main}>
-            <input 
-                type='text'
-                className={s.searchBox}
-                autoComplete='false'
-                placeholder='Search...'
-                onChange={(e) => setInput(e.target.value)}
-                onKeyUp={async (e) => { handleEvent(e) }}>
-            </input>
+            <div className={s.searchBox}>    
+                <input 
+                    type='text'
+                    autoComplete='false'
+                    placeholder='Search...'
+                    onChange={(e) => setInput(e.target.value)}
+                    onKeyUp={async (e) => { handleEvent(e) }}>
+                </input>
+            </div>
 
             <div className={s.location}>
-                <div className={s.name}> {location.name} {location.country} </div>
-                {/* <div className={s.country}> {location.country} </div> */}
+                {location.name && 
+                    <div className={s.name}> {location.name}, {location.country} </div>
+                }
                 <div className={s.date}> {location.localtime} </div>
             </div>
 
